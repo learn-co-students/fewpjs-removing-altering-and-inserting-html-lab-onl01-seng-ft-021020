110137901a42859f357564ba3fdee2a50b6bd6ee
ktalmage <ktalmage85@gmail.com>
2020-05-14 15:06:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+var rm = document.getElementById('main');
+
+rm.remove();
+
+
+    var newHeader = document.createElement("h1");
+    newHeader.id = 'victory';
+
+    var newHeaderInfo = document.createTextNode("KYLE is the champion");
+
+    
+    newHeader.appendChild(newHeaderInfo);
+    
