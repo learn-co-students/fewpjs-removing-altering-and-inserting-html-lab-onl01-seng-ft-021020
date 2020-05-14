251d00ee4b55ac95fdf52fe1281903b78f97bbe8
github username <github email address>
2020-05-13 20:09:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+let main = document.querySelector("#main");
+main.remove();
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "chris-carter is the champion";
